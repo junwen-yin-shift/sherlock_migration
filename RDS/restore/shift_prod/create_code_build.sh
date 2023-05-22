9a61fcfdf40e530a38efd0c0db576557a45fdd64
junwen.yin <junwen.yin.shift@axa.com>
2023-05-22 11:27:47 +0200

    add codebuild deploy script

---
+++ RDS/restore/shift_prod/create_code_build.sh
@@ -0,0 +1,6 @@
+aws codebuild update-project --cli-input-json file://shck-webapp-rds-pg-restore-italy-prod.json
+aws codebuild update-project --cli-input-json file://shck-webapp-rds-pg-restore-quixa-prod.json
+aws codebuild update-project --cli-input-json file://shck-webapp-rds-pg-restore-uk-prod.json
+aws codebuild update-project --cli-input-json file://shck-webapp-rds-pg-restore-ireland-prod.json
+aws codebuild update-project --cli-input-json file://shck-webapp-rds-pg-restore-france-prod.json
+aws codebuild update-project --cli-input-json file://shck-webapp-rds-pg-restore-switzerland-prod.json
